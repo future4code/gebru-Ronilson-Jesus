a73f399dd12f9a1c3030708286ccc9a648dd275a
formigascegas <ronillson-10@hotmail.com>
2022-02-03 23:48:48 -0300

    Aula - Renderização de Listas.

--- modulo3/estados-insta4/insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/insta4/src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import styled from 'styled-components'
 import Post from './components/Post/Post';
-import Foto from './img/imgpost/dobbyPerfil.jpg'
-import FotoDobby from './img/imgpost/dobbyPost.jpg'
-import FotoPedro from './img/imgpost/pedrao.jpg'
 
 const MainContainer = styled.div`
   display: flex;
@@ -13,25 +10,86 @@ const MainContainer = styled.div`
 `
 
 class App extends React.Component {
+  state = {
+    postar: [
+      {
+        nomeUsuario: 'Viagens',
+        fotoUsuario: 'https://picsum.photos/50/50',
+        fotoPost: 'https://picsum.photos/200/150?grayscale'
+      },
+      {
+        nomeUsuario: 'Pedrão Cabral',
+        fotoUsuario: 'https://picsum.photos/50/50',
+        fotoPost: 'https://picsum.photos/200/150?grayscale'
+      },
+      {
+        nomeUsuario: 'Vi',
+        fotoUsuario: 'https://picsum.photos/50/50',
+        fotoPost: 'https://picsum.photos/200/150?grayscale'
+      }
+    ],
+
+    valorInputUsuario: "",
+    valorInputFotoUsuario: "",
+    valorInputPostUsuario: ""
+
+  }
+
+  adicionaPost = () => {
+
+    const novoPost = {
+
+      nomeUsuario: this.state.valorInputUsuario,
+      fotoUsuario: this.state.valorInputFotoUsuario,
+      fotoPost: this.state.valorInputPostUsuario
+
+    }
+    const novosPosts = [...this.state.pessoas, novoPost]
+    this.setState({ pessoas: novosPosts })
+  }
+
+  onChangeInputUsuario = (event) => {
+    this.setState({ valorInputUsuario: event.target.value })
+  }
+
+  onChangeInputFotoUsuario = (event) => {
+    this.setState({ valorInputFotoUsuario: event.target.value })
+  }
+
+  onChangeInputPostUsuario = (event) => {
+    this.setState({ valorInputPostUsuario: event.target.value })
+  }
+
+
   render() {
+    const listaPost = this.state.postar.map((postando) => {
+      return (
+        <Post   
+          nomeUsuario= {postando.nomeUsuario}
+          fotoUsuario= {postando.fotoUsuario}
+          fotoPost= {postando.fotoPost}
+        />
+      )
+
+    })
     return (
       <MainContainer>
-        <Post
-          nomeUsuario={'Dobby'}
-          fotoUsuario={Foto}
-          fotoPost={FotoDobby}
-        />
-        <Post
-          nomeUsuario={'Pedrão Cabral'}
-          fotoUsuario={'https://www.plataformamedia.com/wp-content/uploads/2020/09/16163647226054.jpg'}
-          fotoPost={FotoPedro}
-        />
-        <Post
-          nomeUsuario={'Viagens'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150?grayscale'}
-        />
-      </MainContainer>
+        <input 
+            value={this.state.valorInputUsuario} 
+            onChange={this.onChangeInputUsuario}
+            placeholder={"Nome"}
+          />
+          <input
+            value={this.state.valorInputEmail}
+            onChange={this.onChangeInputEmail}
+            placeholder={"Foto"}
+          />
+          <button onClick={this.adicionaPost}>Postar</button>
+
+
+
+       {listaPost}
+       </MainContainer>
     );
   }
 }
